075f9a9e9ddf8b50250a2d05fe2272603db9f27c
aniruddha_chunne <aniruddha_chunne@calypso.com>
2023-02-05 14:30:09 +0530

    access config from git sr.no.22
    Change-Id: I5e2e890a5ff3d5bc8bd384e115a99647916730cd

---
+++ ConfigServer/src/main/java/com/config/server/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ConfigServerApplication.class, args);
+
+		System.out.println("Config-Server ");
+
+
+	}
+
+}
